dd66f0a3a32cb34bf70e3ccfcaecb1e2d1a2282a
Md. arifur Rahman <coder.roby@gmail.com>
2022-01-01 04:03:01 +0600

    code by own

--- TODO_list/JS/main.js
+++ TODO_list/JS/main.js
@@ -1,21 +1,24 @@
-const submit = document.getElementById('submit');
-const tasks = document.getElementsByClassName('tasks');
 const inputField = document.getElementById('inputField');
-submit.addEventListener('click', addTasks);
+const submit = document.getElementById("submit");
+const task = document.getElementById('task');
 
-function addTasks() {
+submit.addEventListener('click', tasks);
+
+function tasks() {
     let list = document.createElement('li');
     let inputValue = inputField.value;
     if (inputValue) {
         list.innerHTML = inputValue;
-        tasks[0].children[0].appendChild(list);
+        task.firstElementChild.appendChild(list);
         inputField.value = "";
-        list.addEventListener("click",function(){
-            list.style.textDecoration = "line-through"
-        });
-        list.addEventListener("dblclick",function(){
+        list.addEventListener('click',function(){
+            list.style.textDecoration = "line-through";
+            list.style.color = "green";
+        })
+        list.addEventListener('dblclick',function(){
             list.remove();
         })
     }
-    else alert("please input SOmething");
+    else alert('please input something');
+
 }
